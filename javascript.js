ac1fd11b58e4285f0e7868d2e9927de3f809ddeb
Guillermo <Guillermo@Guillermo-PC>
2015-05-20 19:27:37 +0200

    Go, go ,go!!!

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$.ajax({
+        url: "webservice/valida_user.php",
+        type: "POST",
+        data: formulario.serialize(),
+        success : function(data){
+              //php script returns filename
+              //we apply this filename as the value for the hidden field in form2
+              
+              obj = JSON.parse(data);
+
+              if (obj["disponible"]!=0){
+                alert("Usuario logeado!!! User: "+oForm.elements["user"].value);  
+              }
+              else{
+                $('#login').append( "<p style='background:red'>Usuario no existe</p>" );
+              }
+              
+              // $('#form2 #filename').val(filename);
+        }
+      });
---
+++ webservice/index.php
@@ -0,0 +1,39 @@
+<?php
+$host="52.24.210.244:389";
+
+$ds=ldap_connect($host);
+ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+//	$user = $_POST["user"];
+//	$pass = $_POST["pass"];
+        
+$ldap_user="user1@toca.cat";
+$ldap_pass="Platano123$";
+
+if ($ds) { 
+    echo "Vinculando ..."; 
+    $r=ldap_bind($ds, $ldap_user, $ldap_pass);     
+    echo "El resultado de la vinculación es " . $r . "<br />";
+
+    $sr=ldap_search($ds, "DC=toca,DC=cat", "objectClass=group");  
+    echo "El resultado de la búsqueda es " . $sr . "<br />";
+
+    echo "El número de entradas devueltas es " . ldap_count_entries($ds, $sr) . "<br />";
+
+    echo "Obteniendo entradas ...<p>";
+    $info = ldap_get_entries($ds, $sr);
+    echo "Los datos para " . $info["count"] . " objetos devueltos:<p>";
+
+    for ($i=0; $i<$info["count"]; $i++) {
+        echo "El dn es: " . $info[$i]["dn"] . "<br />";
+        echo "El mo es: " . $info[$i]["memberof"][1] . "<br />";
+        echo "La primera entrada cn es: " . $info[$i]["cn"][0] . "<br /><hr />";
+    }
+
+    echo "Cerando la conexión";
+    ldap_close($ds);
+
+} else {
+    echo "Error";
+}
+?>
